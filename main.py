ba09c4f4b15f4f20609abff8460a2b1cc11e355f
Catalin Avram <papiot@gmail.com>
2018-12-16 15:25:29 +0100

    Try 12 epochs

--- main.py
+++ main.py
@@ -250,7 +250,7 @@ def run():
 
         # TODO: Train NN using the train_nn function
 
-        epochs = 24 # 6 12 24 48
+        epochs = 12 # 6 12 24 48
         batch_size = 5
         
         saver = tf.train.Saver()
